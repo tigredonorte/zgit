03a072cf6b3ab4e455ed0424ba24d9e248486a08
Thompson <tigredonorte3@gmail.com>
2023-10-24 18:59:55 -0300

    alter branch.sh

--- src/branch.sh
+++ src/branch.sh
@@ -31,18 +31,23 @@ main() {
 
     local current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-    if branch_exists "$current_branch" "$slug"; then
-        echo "A branch matching the pattern ${current_branch}-{0..n}-${slug} already exists. Aborting."
-        exit 1
+    local next_branch_name
+    if [[ "$current_branch" == "main" ]]; then
+        next_branch_name="${slug}"
+        if branch_exists "$current_branch" "$slug"; then
+            echo "A branch with slug ${slug} already exists. Aborting."
+            exit 1
+        fi
+    else
+        next_branch_name="${current_branch}-1-${slug}"
+        if branch_exists "$current_branch" "$slug"; then
+            echo "A branch matching the pattern ${current_branch}-{0..n}-${slug} already exists. Aborting."
+            exit 1
+        fi
     fi
 
-    # If no matching branch is found, create a new branch
-    local next_branch_name="${current_branch}-1-${slug}"
     git branch "$next_branch_name"
     git checkout "$next_branch_name"
-
-    # Update the PR name with the slug
-    # ... (you would need to implement the logic for updating the PR name here)
 }
 
 # If the script is called with a function name, execute that function
